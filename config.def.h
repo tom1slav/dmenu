245e3f7e8cfc00b177444e230288b7c38ccd87fe
Tomislav Nikic <tomislav.nikic@fhwn.ac.at>
2022-09-20 12:21:09 +0200

    adding lineheight
    This adds two variables: lineheight and min_lineheight.
    The later is only used in combination with -h.

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,7 +14,10 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 40;
+static unsigned int min_lineheight = 40;
 
 /*
  * Characters not considered part of a word while deleting words
